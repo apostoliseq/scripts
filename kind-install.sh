db32b88b380dbf828381adb3d10374901eac5eb3
apostoliseq <86881584+apostoliseq@users.noreply.github.com>
2025-05-17 16:25:05 +0300

    Create kind-install.sh

---
+++ kind-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Which version to install (number only)?"
+read version
+
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v${version}/kind-$(uname)-amd64
+
+chmod u+x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod u+x kubectl
+sudo mv kubectl /usr/local/bin/kubectl
