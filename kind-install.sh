878e997e96e0470e4833d4b190dc4ef5d957e86a
apostoliseq <86881584+apostoliseq@users.noreply.github.com>
2025-05-19 07:59:18 +0300

    Update kind-install.sh

--- kind-install.sh
+++ kind-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Which version to install (number only)?"
-read version
+read VERSION
 
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v${version}/kind-$(uname)-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v${VERSION}/kind-$(uname)-amd64
 
 chmod u+x ./kind
 sudo mv ./kind /usr/local/bin/kind
